bb5134590384c92ec50ed49ee32f864f9e282137
adri006 <adrixan.mmairambekov@gmail.com>
2023-11-07 16:03:05 +0300

    Add functionality using js

---
+++ script.js
@@ -0,0 +1,82 @@
+// const searchForm = document.getElementById('search-form')
+// const searchInput = document.getElementById('search-input')
+// const resultsEl = document.getElementById('results')
+
+// searchForm.addEventListener('submit',function (e) {
+//     e.preventDefault()
+//     const q = searchInput.value
+
+//     search(q)
+// })
+
+// function search(q) {
+//     const apiKey = 'ZaLweRDNzutCPXYOttmClwCqwMRbjybf'
+//     const path = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${q}`
+
+//     fetch(path).then(function(res) {
+//         return res.json()
+//     }).then(function (json) {
+//         console.log(json.data[0].images.fixed_width.url)
+//         let resultsHTML = ''
+
+//         json.data.forEach(function(obj) {
+//             console.log(obj)
+
+//             const url = obj.images.fixed_width.url
+//             const title = obj.title
+
+//             resultsHTML += `<div><img 
+//                 src="${url}" 
+//                 class="gif-img"
+//                 alt="${title}"
+//                 ></div> `
+//         })
+
+//         resultsEl.innerHTML = resultsHTML
+//     }).catch(function (err) {
+//         console.log(err.messege)
+//     })
+// }
+
+
+const searchForm = document.getElementById('search-form');
+const searchInput = document.getElementById('search-input');
+const resultsEl = document.getElementById('results');
+
+function search(q) {
+    const apiKey = 'ZaLweRDNzutCPXYOttmClwCqwMRbjybf';
+    const path = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${q}`;
+
+    fetch(path)
+        .then(function (res) {
+            return res.json();
+        })
+        .then(function (json) {
+            let resultsHTML = '';
+
+            json.data.forEach(function (obj) {
+                const url = obj.images.fixed_width.url;
+                const title = obj.title;
+
+                resultsHTML += `<div><img 
+                src="${url}" 
+                class="gif-img"
+                alt="${title}"
+                ></div> `;
+            });
+
+            resultsEl.innerHTML = resultsHTML;
+        })
+        .catch(function (err) {
+            console.log(err.message);
+        });
+}
+
+search('jdm');
+
+searchForm.addEventListener('submit', function (e) {
+    e.preventDefault();
+    const q = searchInput.value;
+
+    search(q);
+});
